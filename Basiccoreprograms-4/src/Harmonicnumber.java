7310b1537b208e5ebd7c2d57d228a016d505d957
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-03-12 17:57:43 +0530

    UC4

---
+++ Basiccoreprograms-4/src/Harmonicnumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Harmonicnumber {
+	float harmonicNumber(int num){
+		float harmonicFormula=0;
+		for(float i=2;i<num;i++) {
+			harmonicFormula=(1/i)+harmonicFormula;
+		}
+		return harmonicFormula;
+}
+	public static void main(String[] args) {
+		Harmonicnumber rv=new Harmonicnumber();
+		Scanner inp=new Scanner(System.in);
+		System.out.println("Enter A Number Whose Harmonic Value u need");
+		int num=inp.nextInt();
+		if (num>0)
+		System.out.println("Harmonic value is "+rv.harmonicNumber(num));
+	}
+}
